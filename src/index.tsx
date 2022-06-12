8859e38f6bfe98eb67ee8b47a8b43703aa5e400b
ZherdevMak <Zherdev.mak@gmail.com>
2022-06-12 12:00:00 +0300

    mondey final 63%

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,18 @@
 
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 
+function UsersList() {
+    const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
+    return (
+        <p>Тут будет список пользователей</p>
+    )
+}
+
+ReactDOM.render(
+    <UsersList/>, document.getElementById('root')
+);
+// Что вернёт выражение: typeof setUsers?
+
+   console.log (typeof setUsers)
